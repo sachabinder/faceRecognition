690bcfa222c573047f0cc57d1d250f4dcc16591a
Sacha BINDER <sacha@sachas-laptop.home>
2023-02-28 22:00:02 +0100

    front setup

---
+++ front/webpack.config.js
@@ -0,0 +1,46 @@
+/* eslint-disable */
+
+const path = require('path');
+
+module.exports = {
+    devtool: 'eval-source-map',
+    mode: 'development',
+    entry: {
+        app: './src/app.js',
+    },
+    resolve: {
+        modules: [__dirname, 'node_modules'],
+        extensions: ['.js', '.jsx']
+    },
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'app.bundle.js',
+    },
+    devServer: {
+        historyApiFallback: true,
+        static: path.resolve(__dirname, './public'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                // if use css
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+
+                // if use scss
+                // test: /\.scss$/,
+                // use: ['style-loader', 'css-loader', 'sass-loader']
+
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ]
+    }
+};
