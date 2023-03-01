a1bb12f8763d2e1967d91dd55218c1ce1d9691ba
Sacha BINDER <sacha@sachas-laptop.home>
2023-03-01 12:03:06 +0100

    database and authentificaion containers

---
+++ front/src/components/DatabaseContent.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+
+export default function DatabaseContent() {
+  return (
+    <Paper sx={{ maxWidth: 936, margin: 'auto', overflow: 'hidden' }}>
+      <AppBar
+        position="static"
+        color="default"
+        elevation={0}
+        sx={{ borderBottom: '1px solid rgba(0, 0, 0, 0.12)' }}
+      >
+        <Toolbar>
+          <Grid container spacing={2} alignItems="center">
+            <Grid item xs>
+            </Grid>
+            <Grid item>
+              <Button variant="contained" sx={{ mr: 1 }}>
+                Add user
+              </Button>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      <Typography sx={{ my: 5, mx: 2 }} color="text.secondary" align="center">
+        No users for this project yet
+      </Typography>
+    </Paper>
+  );
+}
