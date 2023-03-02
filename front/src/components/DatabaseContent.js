dc274af78cfbeb4c3163249e3576a357c442ce92
Sacha BINDER <sacha@Sachas-Laptop.local>
2023-03-02 16:21:27 +0100

    database viewer front

--- front/src/components/DatabaseContent.js
+++ front/src/components/DatabaseContent.js
@@ -6,6 +6,48 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import UserRowCard from './UserRowCard';
+import {DialogContent, DialogTitle, IconButton, Dialog} from '@mui/material';
+import styled from '@emotion/styled';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faClose} from '@fortawesome/free-solid-svg-icons';
+
+const BootstrapDialog = styled(Dialog)(({theme}) => ({
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(2),
+  },
+  '& .MuiDialogActions-root': {
+    padding: theme.spacing(1),
+  },
+}));
+
+/**
+ * Component coding the dialog title.
+ * @param {props} props properties
+ * @return {Component} A component
+ */
+function BootstrapDialogTitle(props) {
+  const {children, onClose, ...other} = props;
+
+  return (
+    <DialogTitle sx={{m: 0, p: 2}} {...other}>
+      {children}
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          onClick={onClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <FontAwesomeIcon icon={faClose} />
+        </IconButton>
+      ) : null}
+    </DialogTitle>
+  );
+}
 
 /**
  * Component coding the content of the database visualisation
@@ -13,30 +55,60 @@ import UserRowCard from './UserRowCard';
  * @return {Component} A component
  */
 export default function DatabaseContent() {
+  const [openEditor, setOpenEditor] = React.useState(false);
+
+  const handleClickOpenEditor = () => {
+    setOpenEditor(true);
+  };
+
+  const handleCloseEditor = () => {
+    setOpenEditor(false);
+  };
+
   return (
-    <Paper sx={{maxWidth: 936, margin: 'auto', overflow: 'hidden'}}>
-      <AppBar
-        position="static"
-        color="default"
-        elevation={0}
-        sx={{borderBottom: '1px solid rgba(0, 0, 0, 0.12)'}}
-      >
-        <Toolbar>
-          <Grid container spacing={2} alignItems="center">
-            <Grid item xs></Grid>
-            <Grid item>
-              <Button variant="contained" sx={{mr: 1}}>
-                Add user
-              </Button>
+    <div>
+      <Paper sx={{maxWidth: 936, margin: 'auto', overflow: 'hidden'}}>
+        <AppBar
+          position="static"
+          color="default"
+          elevation={0}
+          sx={{borderBottom: '1px solid rgba(0, 0, 0, 0.12)'}}
+        >
+          <Toolbar>
+            <Grid container spacing={2} alignItems="center">
+              <Grid item xs></Grid>
+              <Grid item>
+                <Button
+                  variant="contained"
+                  sx={{mr: 1}}
+                  onClick={handleClickOpenEditor}
+                >
+                  Ajouter un utilisateur
+                </Button>
+              </Grid>
             </Grid>
-          </Grid>
-        </Toolbar>
-      </AppBar>
-      <Typography sx={{my: 5, mx: 2}} color="text.secondary" align="center">
-        No users for this project yet
-      </Typography>
-      <UserRowCard />
-      <UserRowCard />
-    </Paper>
+          </Toolbar>
+        </AppBar>
+        <Typography sx={{my: 5, mx: 2}} color="text.secondary" align="center">
+          Il n'y a pas d'utilisateur dans la base de donnée
+        </Typography>
+        <UserRowCard />
+        <UserRowCard />
+      </Paper>
+      <BootstrapDialog
+        onClose={handleCloseEditor}
+        aria-labelledby="customized-dialog-title"
+        open={openEditor}
+        fullWidth
+      >
+        <BootstrapDialogTitle
+          id="customized-dialog-title"
+          onClose={handleCloseEditor}
+        >
+          Éditer un profil
+        </BootstrapDialogTitle>
+        <DialogContent dividers> TODO : Form de profil vide</DialogContent>
+      </BootstrapDialog>
+    </div>
   );
 }
--- front/src/components/UserRowCard.js
+++ front/src/components/UserRowCard.js
@@ -1,8 +1,55 @@
 import React from 'react';
-import {Avatar, Grid, Paper, Typography} from '@mui/material';
+import {
+  Avatar,
+  DialogContent,
+  DialogTitle,
+  Grid,
+  Paper,
+  Typography,
+} from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faPenToSquare} from '@fortawesome/free-solid-svg-icons';
+import {faPenToSquare, faClose} from '@fortawesome/free-solid-svg-icons';
+import styled from '@emotion/styled';
+import Dialog from '@mui/material/Dialog';
+
+const BootstrapDialog = styled(Dialog)(({theme}) => ({
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(2),
+  },
+  '& .MuiDialogActions-root': {
+    padding: theme.spacing(1),
+  },
+}));
+
+/**
+ * Component coding the dialog title.
+ * @param {props} props properties
+ * @return {Component} A component
+ */
+function BootstrapDialogTitle(props) {
+  const {children, onClose, ...other} = props;
+
+  return (
+    <DialogTitle sx={{m: 0, p: 2}} {...other}>
+      {children}
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          onClick={onClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <FontAwesomeIcon icon={faClose} />
+        </IconButton>
+      ) : null}
+    </DialogTitle>
+  );
+}
 
 /**
  * Component coding the user card for the displaying of the database
@@ -10,34 +57,63 @@ import {faPenToSquare} from '@fortawesome/free-solid-svg-icons';
  * @return {Component} A component
  */
 export default function UserRowCard() {
+  const [openEditor, setOpenEditor] = React.useState(false);
+
+  const handleClickOpenEditor = () => {
+    setOpenEditor(true);
+  };
+
+  const handleCloseEditor = () => {
+    setOpenEditor(false);
+  };
+
   return (
-    <Paper elevation={4} sx={{margin: 2, padding: 2}}>
-      <Grid container spacing={2}>
-        <Grid item>
-          <Avatar sx={{height: '60px', width: '60px'}} />
-        </Grid>
-        <Grid item sm container>
-          <Grid item xs container direction="column" spacing={2}>
-            <Grid item>
-              <Typography variant="h6">Sacha BINDER</Typography>
-            </Grid>
-            <Grid item>
-              <Typography color="text.secondary">
-                Le lorem ipsum est, en imprimerie, une suite de mots sans
-                signification utilisée à titre provisoire pour calibrer une mise
-                en page, le texte définitif venant remplacer le faux-texte dès
-                qu'il est prêt ou que la mise en page est achevée. Généralement,
-                on utilise un texte en faux latin, le Lorem ipsum ou Lipsum.
-              </Typography>
-            </Grid>
-          </Grid>
+    <div>
+      <Paper elevation={4} sx={{margin: 2, padding: 2}}>
+        <Grid container spacing={2}>
           <Grid item>
-            <IconButton aria-label="">
-              <FontAwesomeIcon icon={faPenToSquare} />
-            </IconButton>
+            <Avatar sx={{height: '60px', width: '60px'}} />
+          </Grid>
+          <Grid item sm container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item>
+                <Typography variant="h6">Sacha BINDER</Typography>
+              </Grid>
+              <Grid item>
+                <Typography color="text.secondary">
+                  Le lorem ipsum est, en imprimerie, une suite de mots sans
+                  signification utilisée à titre provisoire pour calibrer une
+                  mise en page, le texte définitif venant remplacer le
+                  faux-texte dès qu'il est prêt ou que la mise en page est
+                  achevée. Généralement, on utilise un texte en faux latin, le
+                  Lorem ipsum ou Lipsum.
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <IconButton onClick={handleClickOpenEditor}>
+                <FontAwesomeIcon icon={faPenToSquare} />
+              </IconButton>
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
-    </Paper>
+      </Paper>
+      <BootstrapDialog
+        onClose={handleCloseEditor}
+        aria-labelledby="customized-dialog-title"
+        open={openEditor}
+        fullWidth
+      >
+        <BootstrapDialogTitle
+          id="customized-dialog-title"
+          onClose={handleCloseEditor}
+        >
+          Éditer un profil
+        </BootstrapDialogTitle>
+        <DialogContent dividers>
+          TODO : form de profil pré complété
+        </DialogContent>
+      </BootstrapDialog>
+    </div>
   );
 }
