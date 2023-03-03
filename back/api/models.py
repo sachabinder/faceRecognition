ae6ca908c7d4a7c64841b11ac8c9db3ae58d2892
Sacha BINDER <sacha@sachas-laptop.home>
2023-03-03 11:45:37 +0100

    models and migration

--- back/api/models.py
+++ back/api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+import os
+import uuid
+
+
 # Create your models here.
+def upload_to(instance, filename):
+    img_extension = os.path.splitext(filename)[-1]
+
+    return "profile_picture/" + str(uuid.uuid4()) + img_extension
+
+
+class Profile(models.Model):
+    first_name = models.CharField(max_length=80, blank=False, null=False)
+    last_name = models.CharField(max_length=80, blank=False, null=False)
+    description = models.TextField()
+    profile_picture = models.ImageField(upload_to=upload_to, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.first_name + "-" + self.last_name
