e440f5320ea3bca27b9044fa7226a93584301bc6
Sacha BINDER <sacha@sachas-laptop.home>
2023-03-03 11:46:45 +0100

    url and admin

--- back/api/admin.py
+++ back/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from api.models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name")
